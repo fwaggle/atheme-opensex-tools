e700c17920aeb89b27027695c49e142545954aa3
fwaggle <jamie.f@mumbledog.com>
2014-03-31 16:43:35 +1100

    Nickname and metadata parsing now works. I think.

--- merge.py
+++ merge.py
@@ -7,18 +7,51 @@ if (len(sys.argv) < 2):
     print "Usage: %s <atheme.db>" % sys.argv[0]
     exit()
 
-users = []
+users = {}
 db = open(sys.argv[1], 'r')
 
 for s in db:
     s = s.strip()
     record = s.split(' ', 1)
     
+    # MD: User record
     if record[0] == 'MU':
         u = User(record[1])
-        users.append(u)
+        users[u.name] = u
+
+    # MD: metadata?
+    elif record[0] == 'MD':
+        type = record[1].split(' ', 1)
+        
+        # User metadata
+        if type[0] == 'U':
+            meta = type[1].split(' ', 2)
+            if meta[0] in users:
+                u = users[meta[0]]
+                u.meta[meta[1]] = meta[2]
+            else:
+                print "Warning: Metadata found for nonexistent user: %s" % meta[0]
+
+    # MN: User nick registration
+    elif record[0] == 'MN':
+        nick = record[1].split(' ', 1)
+        
+        if nick[0] in users:
+            u = users[nick[0]]
+            n = Nick(nick[1])
+            u.nicks[n.name] = n
+        else:
+            print "Warning: Nickname registration for nonexistent user: %s" % nick[0]
 
 db.close()
 
+#exit()
+
 for u in users:
-    print "%s (%s) created %d" % (u.name, u.email, u.created)
+    user = users[u]
+    print "%s (%s) created %d" % (user.name, user.email, user.created)
+    print ("\tNicknames: "), # fuck python3
+    for n in user.nicks:
+        print(" %s" % n),
+    print ""
+    print "\t %s" % user.meta
--- opensex/user.py
+++ opensex/user.py
@@ -1,13 +1,17 @@
-class User:
+class User(object):
     name = None
     password = None
     email = None
     created = 0
     seen = 0
     flags = '0 0 0 0'
+    meta = None
+    nicks = None
     
     # create empty user
     def __init__(self):
+        self.meta = {}
+        self.nicks = {}
         pass
     
     # create user from db record
@@ -20,3 +24,17 @@ class User:
         self.created = int(r[3])
         self.seen = int(r[4])
         self.flags = r[5]
+        self.meta = {}
+        self.nicks = {}
+
+class Nick(object):
+    name = None
+    created = 0
+    seen = 0
+    
+    def __init__(self, record):
+        r = record.split(' ')
+        
+        self.name = r[0]
+        self.created = r[1]
+        self.seen = r[2]
