8b67c1bb17304b773eb00205ba6a6b8ac8dd7568
Jamie Fraser <jamie.f@mumbledog.com>
2014-04-01 10:54:34 +1100

    Put back code for reading second DB. Whoops.

--- merge.py
+++ merge.py
@@ -21,13 +21,13 @@ for s in db:
 db.close()
 
 # Read in and parse second database
-#db = open(sys.argv[2], 'r')
-#
-#for s in db:
-#    s = s.strip()
-#    parse(s)
-#
-#db.close()
+db = open(sys.argv[2], 'r')
+
+for s in db:
+    s = s.strip()
+    parse(s)
+
+db.close()
 
 #debug()
 output()
